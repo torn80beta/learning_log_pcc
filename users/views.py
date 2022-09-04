abcdadb1da40f9199cc33fbaefcb6df77650bcfb
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-09-04 12:09:13 +0300

    User registration function has been added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register(request):
+    """Регистрация нового пользователя"""
+    if request.method != 'POST':
+        # Вывод пустой формы регистрации
+        form = UserCreationForm()
+    else:
+        # Обработка заполненной формы
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Выполнение входа и перенаправление на домашнюю страницу
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Вывод пустой или недействительной формы
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
